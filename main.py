7e530e661ea91d12f36453a92fd1c9af7eede9ae
mattisalan <matti.salan@gmail.com>
2023-05-29 11:01:56 +0300

    Create game screen

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import pygame
 
-# Starting a python game 
+# game setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+running = True
 
+while running:
+
+    # check events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
