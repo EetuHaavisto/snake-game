762dc0c1e6b5c86d02f10a80dc42023069082c59
EetuHaavisto <eetu.haavisto@elisanet.fi>
2023-05-29 12:05:07 +0300

    Initialize snake object

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
-
+from snake import *
 # game setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 running = True
+
+snake = Snake(screen.get_width()/2, screen.get_height()/2,"white")
 
 while running:
 
@@ -11,5 +13,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.fill("black")
+    snake.draw_snake(screen)
 
+
+    pygame.display.flip() # Updates full screen, .update() updates part of the screen
 pygame.quit()
