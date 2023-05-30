83c3631ed3a9593cf464c7acc6d004c71349279b
EetuHaavisto <eetu.haavisto@elisanet.fi>
2023-05-30 11:09:33 +0300

    Add method to get head coordinates

--- snake.py
+++ snake.py
@@ -18,8 +18,12 @@ class Snake:
 
         self.head.centerx = old_ctr_x + delta_x
         self.head.centery = old_ctr_y + delta_y
+        # Add (fill with green) the new snake head
         pygame.draw.rect(surface=screen, color="green", rect=self.head)
 
-
+    def get_head_center_coord(self):
+        x = self.head.centerx
+        y = self.head.centery
+        return (x,y)
 
 
