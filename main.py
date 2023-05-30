5549cdb6f94b725744aed40dd578c48df5165d5e
EetuHaavisto <eetu.haavisto@elisanet.fi>
2023-05-30 11:57:24 +0300

    Add movement to all 4 directions

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ Initialize a window or screen for display. This 'display surface' represents the
 screen = pygame.display.set_mode((800, 800))
 running = True
 
-
+SPEED = 2*SNAKE_RADIUS
 MOVE = pygame.USEREVENT+0
 clock = pygame.time.Clock()
 pygame.time.set_timer(MOVE,500)
 snake = Snake(screen.get_width()/2-SNAKE_RADIUS, screen.get_height()/2+SNAKE_RADIUS)
-
+# Default movement to the right:
+dir = "r" # up, down "d", left "l" or right "r"
+delta_x = SPEED
+delta_y = 0
 screen.fill("black")
 
 while running:
@@ -25,10 +28,24 @@ while running:
             running = False
 
         if event.type == MOVE:
-            snake.move_snake(screen,2*SNAKE_RADIUS,0)
+            snake.move_snake(screen, delta_x, delta_y)
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and dir != "d":
+                delta_y = -SPEED; delta_x = 0;
+                dir = "u"
 
+            if event.key == pygame.K_DOWN and dir != "u":
+                delta_y = SPEED; delta_x = 0;
+                dir = "d"
 
+            if event.key == pygame.K_LEFT and dir != "r":
+                delta_y = 0; delta_x = -SPEED;
+                dir = "l"
+
+            if event.key == pygame.K_RIGHT and dir != "l":
+                delta_y = 0; delta_x = SPEED;
+                dir = "r"
     pygame.display.update()
     #pygame.display.flip() # Updates full screen, .update() updates part of the screen
     clock.tick(60)
