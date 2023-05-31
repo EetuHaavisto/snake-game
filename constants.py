437db7db6e4c27866b69c9893a2b4c0847c41d73
mattisalan <matti.salan@gmail.com>
2023-05-31 20:46:36 +0300

    Add constants for colors

--- constants.py
+++ constants.py
@@ -1,11 +1,19 @@
 # Width of the snake and food
 SNAKE_WIDTH = 20
 FOOD_WIDTH = 10
+
 # The velocity of the snake
-SNAKE_SPEED = SNAKE_WIDTH / 4
+SNAKE_SPEED = SNAKE_WIDTH
+
 # Screen dimensions
 SCREEN_WIDTH = 600
 SCREEN_HEIGTH = 600
 
+# Colors
+GREEN_HEAD = (77, 217, 17)
+GREEN_BODY = (168, 240, 31)
+RED = (240, 55, 19)
+GREY = (58, 59, 60)
+
 # Fps (frames per second, one while loop refers to one frame)
-FPS = 60
+FPS = 20
