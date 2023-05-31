bd8aaf4171b243b80a8790e0bef84e099909df23
mattisalan <matti.salan@gmail.com>
2023-05-31 09:32:38 +0300

    rebuild main

--- main.py
+++ main.py
@@ -1,21 +1,73 @@
 import pygame
-from snake import *
-# game setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-running = True
+from constants import *
+from snake import Snake
+from food import Food
 
-snake = Snake(screen.get_width()/2, screen.get_height()/2,"white")
+def main():
+    pygame.init()
 
-while running:
+    # Game window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
+    pygame.display.set_caption("Snake")
 
-    # check events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    screen.fill("black")
-    snake.draw_snake(screen)
+    # Frame rate
+    clock = pygame.time.Clock()
+    fps = 60
 
+    # Help variables
 
-    pygame.display.flip() # Updates full screen, .update() updates part of the screen
-pygame.quit()
+    # Initialize game objects
+    snake_head = Snake()
+    food = Food()
+
+    # sprite.Group for snake body
+    snake_body_group = pygame.sprite.Group()
+    snake_body_group.add(snake_head)
+
+    #sprite.Group for food
+    food_group = pygame.sprite.Group()
+    food_group.add(food)
+    
+    # Game loop
+    running = True
+    while running:
+        
+        # Events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            # User presses LEFT/RIGHT/UP/DOWN
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    snake_head.dir = "LEFT"
+                elif event.key == pygame.K_RIGHT:
+                    snake_head.dir = "RIGHT"
+                elif event.key == pygame.K_UP:
+                    snake_head.dir = "UP"
+                elif event.key == pygame.K_DOWN:
+                    snake_head.dir = "DOWN"
+        
+        # Move snake head
+        snake_head.update()
+
+        # If snake eats food
+        if snake_head.rect.colliderect(food.rect):
+            food.move_food()
+
+        # Backgroud / Erase previous frame
+        screen.fill("grey")
+
+        # Draw sprites
+        snake_body_group.draw(screen)
+        food_group.draw(screen)
+
+        # Update display
+        pygame.display.flip()
+
+        clock.tick(fps)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
